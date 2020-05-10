3d6f38381e20599e16be7a14e7a014e2cb371c4b
Llandy Riveron Del Risco <llandy3d@gmail.com>
2020-05-10 21:09:33 +0200

    config: getting or creating config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tofi.config import ensure_config_paths
+from tofi.config import ensure_config_paths, get_or_create_config
 
 
 @pytest.fixture
@@ -26,3 +26,30 @@ def test_ensure_config_path_not_fail_if_paths_exists(mock_paths):
     ensure_config_paths()
 
     assert all(path.exists() for path in mock_paths)
+
+
+def test_get_or_create_creates_config(monkeypatch, tmp_path):
+    CONFIG_PATH = tmp_path / 'tofi.ini'
+    monkeypatch.setattr('tofi.config.CONFIG_PATH', CONFIG_PATH)
+
+    get_or_create_config()
+
+    assert CONFIG_PATH.exists()
+
+
+def test_get_or_create_retrieves_config(monkeypatch, tmp_path):
+    CONFIG_PATH = tmp_path / 'tofi.ini'
+    monkeypatch.setattr('tofi.config.CONFIG_PATH', CONFIG_PATH)
+
+    data = """[test]
+              data = data
+    """
+
+    with open(CONFIG_PATH, 'w') as f:
+        f.write(data)
+
+    config = get_or_create_config()
+    breakpoint()
+
+    assert config.sections()[0] == 'test'
+    assert config['test']['data'] == 'data'
--- tofi/config.py
+++ tofi/config.py
@@ -1,3 +1,4 @@
+import configparser
 from pathlib import Path
 
 
@@ -10,3 +11,22 @@ def ensure_config_paths():
     """Ensures that all config paths needed for execution are created."""
     BASE_PATH.mkdir(parents=True, exist_ok=True)
     SECRETS_PATH.mkdir(exist_ok=True)
+
+
+def get_or_create_config():
+    """Returns the config if it exists, else creates a new file and returns the config."""
+    config = configparser.ConfigParser()
+
+    if CONFIG_PATH.exists():
+        config.read(CONFIG_PATH)
+        return config
+
+    config['backup'] = {
+        'enabled': 'false',
+        'repository': '',
+    }
+
+    with open(CONFIG_PATH, 'w') as f:
+        config.write(f)
+
+    return config
